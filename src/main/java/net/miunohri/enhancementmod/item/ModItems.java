4028cf12cd36b6962cc951b447587fc55c4e1664
gina <ginameiyee@gmail.com>
2025-09-22 23:19:28 -0400

    Initial commit

---
+++ src/main/java/net/miunohri/enhancementmod/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.miunohri.enhancementmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.miunohri.enhancementmod.EnhancementMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EnhancementMod.MOD_ID);
+
+    public static final RegistryObject<Item> ENHANCEMENT_STONE = ITEMS.register("enhancement_stone", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
